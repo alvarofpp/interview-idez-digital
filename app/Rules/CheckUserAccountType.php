d117b0bc17852c7f11cdf71380c7ad798d6f4614
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-07-05 16:20:27 -0300

    Checks whether the user already has an account of a specific type

---
+++ app/Rules/CheckUserAccountType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Checks whether the user already has an account of a specific type.
+ *
+ * Class CheckUserAccountType
+ * @package App\Rules
+ */
+class CheckUserAccountType implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $user = Auth::user();
+
+        $hasAccount = $user->accounts()
+            ->where('account_type_id', $value)
+            ->exists();
+
+        return !$hasAccount;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('rules.CheckUserAccountType');
+    }
+}
---
+++ resources/lang/en/rules.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Structure
+|--------------------------------------------------------------------------
+|
+| Rule (like CPF, etc) - return message
+|
+*/
+
+return [
+    'CheckUserAccountType' => 'User already has such an account.',
+];
